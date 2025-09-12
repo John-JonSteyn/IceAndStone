dd8c2c85998862ae461682fc9086ffb3a0b8ada5
John-Jon Steyn <steyn@sozolabs.com>
2025-09-12 07:54:26 +0400

    Add Session Service
    +Add Session Service.

--- API/Services/Abstractions/ISessionService.cs
+++ API/Services/Abstractions/ISessionService.cs
@@ -1,6 +1,11 @@
-﻿namespace IceAndStone.API.Services.Abstractions
+﻿using IceAndStone.API.Requests;
+using IceAndStone.API.Responses;
+
+namespace IceAndStone.API.Services.Abstractions
 {
     public interface ISessionService
     {
+        Task<SessionResponse> StartAsync(StartSessionRequest request, CancellationToken cancellationToken);
+        Task<SessionResponse> EndAsync(EndSessionRequest request, CancellationToken cancellationToken);
     }
 }

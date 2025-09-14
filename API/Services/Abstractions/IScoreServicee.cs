770757517d6dc9e529575fdccc7feb0d41cadc0c
John-Jon Steyn <steyn@sozolabs.com>
2025-09-14 20:28:21 +0400

    Add Score Service
    +Add Score Service.

--- API/Services/Abstractions/IScoreServicee.cs
+++ API/Services/Abstractions/IScoreServicee.cs
@@ -1,6 +1,10 @@
-﻿namespace IceAndStone.API.Services.Abstractions
+﻿using IceAndStone.API.Requests;
+using IceAndStone.API.Responses;
+
+namespace IceAndStone.API.Services.Abstractions
 {
     public interface IScoreService
     {
+        Task<ScoreResponse> PostTeamScoreAsync(PostTeamScoreRequest request, CancellationToken cancellationToken);
     }
 }
